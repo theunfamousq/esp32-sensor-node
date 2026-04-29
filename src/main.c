400a4b6ada336bca9f5354a616f6c3565f506d93
Quentin ROUSSEY <themoirage@gmail.com>
2026-04-29 15:45:12 +0200

    Create a minimalistic app_main to blink GPIO 2 and write logs

--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
-void app_main() {}
+#include <driver/gpio.h>
+
+#include "esp_log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    ESP_LOGI("main,init", "Initialize GPIO");
+    gpio_reset_pin(GPIO_NUM_2);
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+
+    ESP_LOGI("main,blink,start", "Start blinking on GPIO 2");
+    while (true)
+    {
+        gpio_set_level(GPIO_NUM_2, 1);
+        ESP_LOGI("main,blink", "ON");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        gpio_set_level(GPIO_NUM_2, 0);
+        ESP_LOGI("main,blink", "OFF");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
